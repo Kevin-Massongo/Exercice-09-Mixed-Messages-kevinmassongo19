bf2ae25e8871205664292c5a1c62ee1a3bf52da6
Kevin MASSONGO <kevinmassongo196@gmail.com>
2023-07-28 17:04:33 +0100

    add

---
+++ index.js
@@ -0,0 +1,24 @@
+let aleatoire = Math.floor(Math.random()* 6);
+let aleatoire_un = Math.floor(Math.random()* 6);
+switch (aleatoire) {
+    case 0:
+        console.log('Felix Tshisekedi est le president');
+        break;
+    case 1:
+        console.log('Améliorez votre santé');
+        break;
+    case 2 :
+        console.log('Cristiano est le meilleur joueur du monde');
+        break;
+    case 3:
+        console.log('Kinshasa est la meilleure capitale du monde');
+        break;
+    case 4:
+        console.log('Harry Kane n\'a  une ligue de champion 🤣😂');
+        break;
+    case 5:
+        lo
+default:
+        console.log('Oups y\'a rien');
+    break;
+}
